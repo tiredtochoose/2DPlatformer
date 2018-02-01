3e74566248c3bf6d6a551a667f032c61660c66d4
Lebedev Ivan <ilebedev@Saber3D.ru>
2018-02-01 18:02:09 +0300

    Label and bar works sliderValue does not work

---
+++ Assets/Scripts/Lesson5_HUD.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson5_HUD : MonoBehaviour {
+    
+    private Lesson5_PlayerHealth PlayerHP;
+    private float sliderValue;
+    public float playerHP_Max;
+
+
+
+    void OnGUI()
+    {
+        //PlayerHP = transform.root.GetComponent<Lesson5_PlayerHealth>();
+        //sliderValue = PlayerHP.PlayersHealth;
+
+        GUI.BeginGroup(new Rect(Screen.width / 2 - 100, 10, 200, 100));
+        GUI.Label(new Rect(0, 0, 50, 20), "Health: ");
+        sliderValue = GUI.HorizontalSlider(new Rect(50, 7, 100, 30), sliderValue, 0.0f, playerHP_Max);
+        GUI.EndGroup();
+        
+    }
+
+}
---
+++ Assets/Scripts/Lesson5_PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson5_PlayerHealth : MonoBehaviour {
+
+    [HideInInspector]
+    public float PlayersHealth = 50.0f;
+
+    // Use this for initialization
+
+    public void PlayerHurt(float Dam)
+    {
+        PlayersHealth -= Dam;
+    }
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
