d5502e109d59a539485f05055b1fdc710916e925
tiredtochoose <uven@list.ru>
2018-02-03 17:18:24 +0300

    receiving damage die health bar

--- Assets/Scripts/Lesson5_PlayerHealth.cs
+++ Assets/Scripts/Lesson5_PlayerHealth.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class Lesson5_PlayerHealth : MonoBehaviour {
 
-    [HideInInspector]
-    public float PlayersHealth = 50.0f;
+
+    public float PlayersHealth;
+
+    private float sliderValue;
+    public float playerHP_Max = 10.0f;
 
     // Use this for initialization
 
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void ReceivingDamage(float dmg)
+    {
+        PlayersHealth -= dmg;
+        if (PlayersHealth <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+    void OnGUI()
+    {
+
+        sliderValue = PlayersHealth;
+        print(sliderValue);
+
+        GUI.BeginGroup(new Rect(Screen.width / 2 - 100, 10, 200, 100));
+        GUI.Label(new Rect(0, 0, 50, 20), "Health: ");
+        sliderValue = GUI.HorizontalSlider(new Rect(50, 7, 100, 30), sliderValue, 0.0f, playerHP_Max);
+        GUI.EndGroup();
+
+    }
+
+   
 }
