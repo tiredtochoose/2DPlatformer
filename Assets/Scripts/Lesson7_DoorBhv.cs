188194f82f69b87a50fc6875c2e1cd07bac681f7
afatina <afatina@saber3d.ru>
2018-02-12 11:18:15 +0300

    Falling down bhv added (rigidbody)

--- Assets/Scripts/Lesson7_DoorBhv.cs
+++ Assets/Scripts/Lesson7_DoorBhv.cs
@@ -4,27 +4,23 @@ using UnityEngine;
 
 public class Lesson7_DoorBhv : MonoBehaviour {
 
-    private GameObject lever;
-    private bool leverClosed;
+    //private GameObject lever;
+    //private bool leverClosed;
     private GameObject player;
-    private Animator anim;
+    private float speed = 2.0f;
 
     // Use this for initialization
     void Start()
     {
-        anim = GetComponent<Animator>();
+       
         player = GameObject.FindGameObjectWithTag("Dragon");
-        lever = GameObject.FindGameObjectWithTag("Lever");
+        //lever = GameObject.FindGameObjectWithTag("Lever");
     }	
 	// Update is called once per frame
 	void Update () {
-
-        leverClosed = lever.GetComponent<Lesson6_LeverBhv>().closed;
-
+        
         if (player.transform.position.x > 22.0f)
-             anim.SetBool("Down", true);
-
-        //if (!leverClosed)
-        //    anim.SetBool("Down", false);
+           GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+       
 	}
 }
