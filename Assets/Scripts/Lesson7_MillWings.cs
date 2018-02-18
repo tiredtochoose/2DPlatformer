555a29036f6f2eeebf631667fe27f13d256d809b
tiredtochoose <uven@list.ru>
2018-02-19 01:49:55 +0300

    level update

---
+++ Assets/Scripts/Lesson7_MillWings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson7_MillWings : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+    void FixedUpdate()
+    {
+        transform.RotateAround(transform.position, Vector3.back, 15* Time.deltaTime);
+    }
+}
