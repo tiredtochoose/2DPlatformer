373e2bb6b0127e9e45b1c5ae27b23e108a5ef02e
afatina <afatina@saber3d.ru>
2018-02-20 18:54:53 +0300

    Cave

---
+++ Assets/Scripts/Lesson6_Spikes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson6_Spikes : MonoBehaviour {
+
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
