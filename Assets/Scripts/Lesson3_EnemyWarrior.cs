6453d14759f0f3f2cb6f96635a6d31c91c9551aa
afatina <afatina@saber3d.ru>
2018-02-21 19:17:38 +0300

    ray cast

--- Assets/Scripts/Lesson3_EnemyWarrior.cs
+++ Assets/Scripts/Lesson3_EnemyWarrior.cs
@@ -8,15 +8,14 @@ public class Lesson3_EnemyWarrior : MonoBehaviour {
     public int Health; //Задаем количество жизней через инспект
     public float Speed; // Задаем скорость 
 
-   // private GameObject AttackTarget; // цель врага (в таргет крадем префаб плеера)
     private bool Angry; //видит ли нас противник     
     private bool facingRight = true; // смотрит вперед
     private float DistanceToPlayer;
-    public float AttackDist; //минимальное расстояние, с которого враг может атаковать
-    public float MaxFollowDist; //расстояние, на котором враг теряет игрока из виду
+    public float attackDist; //минимальное расстояние, с которого враг может атаковать
+    public float iCanSeeYou; //расстояние, на котором враг теряет игрока из виду
     public GameObject SpawnPos; //точка возврата врага, если он теряет игрока из виду
     private GameObject player;
-    
+
     private GameObject Warrior_body;
 
     // public GameObject Projectile; // снаряд врага
@@ -34,50 +33,44 @@ public class Lesson3_EnemyWarrior : MonoBehaviour {
         Warrior_body = GameObject.FindGameObjectWithTag("Warrior_body");
         anim = Warrior_body.GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Dragon");
-        
+
     }
-    
+
     // Update is called once per frame
     void Update() {
-        
-       
+
+
     }
-    
+
     void FixedUpdate()
     {
-        DistanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-        //print("Warrior - Player distance is" + DistanceToPlayer);
+        //RaycastHit2D Spawn = Physics2D.Raycast(SpawnPos.transform.position, Vector2.up, 1 << 10);
+        //Debug.DrawRay(SpawnPos.transform.position, Vector2.up, Color.yellow);
 
-        if (DistanceToPlayer <= MaxFollowDist)
+        RaycastHit2D attackray = Physics2D.Raycast(transform.position, Vector2.left, attackDist, 1 << 9);
+        Debug.DrawRay(transform.position, Vector2.right, Color.red);
+
+        RaycastHit2D rightRay = Physics2D.Raycast(transform.position, Vector2.right, iCanSeeYou, 1 << 9);
+        RaycastHit2D leftRay = Physics2D.Raycast(transform.position, Vector2.left, iCanSeeYou, 1 << 9);
+
+        Debug.DrawRay(transform.position, Vector2.right * iCanSeeYou, Color.cyan);
+        Debug.DrawRay(transform.position, Vector2.left * iCanSeeYou, Color.blue);
+
+        if (leftRay.collider && facingRight)
         {
-            //AttackTarget = player;
-            Angry = true; // меняем состояние Angry на true, то есть противник на видит
+            print("Warrior sees left " + leftRay.collider.gameObject.name);
+            Flip();
         }
-        
-        
-        if (Angry) // если игрок зашел в зону видимости врага
+
+        if (rightRay.collider && !facingRight)
         {
-            
-            float x = DirectionDeterm(player); // определяем с какой стороны игрок: если х < 0, то слева, если х > 0, то справа
-            if (x < 0 && facingRight) //если игрок слева, а враг смотрит направо
-                Flip(); //разворачивается
-            else if (x > 0 && !facingRight) //если игрок справв, а враг смотрит налево
-                Flip(); //разворачивается
+            print("Warrior sees right " + rightRay.collider.gameObject.name);
+            Flip();
+        }
 
-            //проверяем дистанцию до игрока, eсли дистанция позволяет атакуем
-            if (DistanceToPlayer <= AttackDist)
-                Attack(); // атакуем
-
-
-            else if (DistanceToPlayer > AttackDist && DistanceToPlayer < MaxFollowDist)
-                Chase(player, Speed * Time.fixedDeltaTime); // Если враг слишком далеко от игрока, идем к нему
-            
-            //если же игрок убежал от врага, враг возвращается на свою позицию
-            if (DistanceToPlayer > MaxFollowDist)
-                Angry = !Angry;           
-        }       
-
-        if (!Angry) // возвращени на место >>>>>>все работает<<<<<<<
+        if (leftRay.collider || rightRay.collider)
+            Chase(player, Speed * Time.fixedDeltaTime);
+        else if (!leftRay.collider && !rightRay.collider)
         {
             float x = DirectionDeterm(SpawnPos); //определяем в каком направлении SpawnPos
             if (x < 0 && facingRight) //если SpawnPos слева, а враг смотрит направо
@@ -85,17 +78,68 @@ public class Lesson3_EnemyWarrior : MonoBehaviour {
             else if (x > 0 && !facingRight) //если SpawnPos справв, а враг смотрит налево
                 Flip(); //разворачивается
 
-            // враг возвращается на свою позицию
-
+            //    // враг возвращается на свою позицию
+            
             transform.position = Vector3.MoveTowards(transform.position, SpawnPos.transform.position, Speed * Time.fixedDeltaTime); //с этим методом перс стоит в айдле
+            
             if (Mathf.Abs(transform.position.x - SpawnPos.transform.position.x) < 0.1)
                 anim.SetFloat("Walk_float", 0);
             //Chase(SpawnPos, Speed * Time.fixedDeltaTime); //если вызывать chase, то сразу включается анимация ходьбы
         }
+
+        if (attackray.collider)
+            Attack();
+        
+        //DistanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        ////print("Warrior - Player distance is" + DistanceToPlayer);
+
+        //if (DistanceToPlayer <= iCanSeeYou)
+        //{
+        //    //AttackTarget = player;
+        //    Angry = true; // меняем состояние Angry на true, то есть противник на видит
+        //}
+
+        //if (Angry) // если игрок зашел в зону видимости врага
+        //{
+
+        //    float x = DirectionDeterm(player); // определяем с какой стороны игрок: если х < 0, то слева, если х > 0, то справа
+        //    if (x < 0 && facingRight) //если игрок слева, а враг смотрит направо
+        //        Flip(); //разворачивается
+        //    else if (x > 0 && !facingRight) //если игрок справв, а враг смотрит налево
+        //        Flip(); //разворачивается
+
+        //    //проверяем дистанцию до игрока, eсли дистанция позволяет атакуем
+        //    if (DistanceToPlayer <= AttackDist)
+        //        Attack(); // атакуем
+
+        //    else if (DistanceToPlayer > AttackDist && DistanceToPlayer < iCanSeeYou)
+        //        Chase(player, Speed * Time.fixedDeltaTime); // Если враг слишком далеко от игрока, идем к нему
+
+        //    //если же игрок убежал от врага, враг возвращается на свою позицию
+        //    if (DistanceToPlayer > iCanSeeYou)
+        //        Angry = !Angry;           
+        //}       
+
+        //if (!Angry) // возвращени на место >>>>>>все работает<<<<<<<
+        //{
+        //    float x = DirectionDeterm(SpawnPos); //определяем в каком направлении SpawnPos
+        //    if (x < 0 && facingRight) //если SpawnPos слева, а враг смотрит направо
+        //        Flip(); //разворачивается
+        //    else if (x > 0 && !facingRight) //если SpawnPos справв, а враг смотрит налево
+        //        Flip(); //разворачивается
+
+        //    // враг возвращается на свою позицию
+
+        //    transform.position = Vector3.MoveTowards(transform.position, SpawnPos.transform.position, Speed * Time.fixedDeltaTime); //с этим методом перс стоит в айдле
+        //    if (Mathf.Abs(transform.position.x - SpawnPos.transform.position.x) < 0.1)
+        //        anim.SetFloat("Walk_float", 0);
+        //    //Chase(SpawnPos, Speed * Time.fixedDeltaTime); //если вызывать chase, то сразу включается анимация ходьбы
+        //}
     }
 
     private void Chase(GameObject chaseObj, float chaseSpeed)
     {
+        //print("Chase speed " + chaseSpeed);
         anim.SetFloat("Walk_float", chaseSpeed);
         transform.position = Vector3.MoveTowards(transform.position, chaseObj.transform.position, chaseSpeed);
     }
@@ -141,8 +185,7 @@ public class Lesson3_EnemyWarrior : MonoBehaviour {
         if (!Cooldown)//если враг сейчас не перезаряжается, то 
         {
             Cooldown = true; // на перезарядке
-            Invoke("HurtPlayer", ReloadTime); // с задержкой вызываем функцию релоад
-                                              // Instantiate(Projectile, parent);
+            Invoke("HurtPlayer", ReloadTime); // с задержкой вызываем функцию релоад            
         }
 
     }
